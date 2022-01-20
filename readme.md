47e98ab434a253c86ebb33ebf0ef293f6c5358e9
chucklapress <charleslapress@gmail.com>
2022-01-19 21:59:56 -0500

    initial commit

---
+++ readme.md
@@ -0,0 +1,31 @@
+# Flask webscrapper
+
+Simple implementation of python flask application  
+using web scraping to pull content from another hosted site  
+and rendering it
+
+## Installation
+clone code base  
+set up project by creating a virtual environment  
+and running
+
+```bash
+pip install -r requirements.txt
+```
+
+## Usage
+
+```python
+python webscraper.py
+
+```  
+results will be rendered on a http connection at:  
+
+http://192.168.1.13:8080/  
+
+## Contributing
+This code is primarily for the purpose of demonstrating simplified python  
+web deployment using flask and a simple wsgi library
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ webscraper.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+from bs4 import BeautifulSoup
+import requests
+import datetime
+
+
+
+source = requests.get('http://lunarosa.herokuapp.com')
+soup = BeautifulSoup(source.text, 'html.parser')
+rows = soup.find_all('h2')
+#for row in rows:
+#    print(row.get_text())
+
+app = Flask(__name__)
+@app.route('/')
+
+
+def index():
+    now = datetime.datetime.now()
+    date = now.strftime("%m-%d-%Y")
+    rows = soup.find_all('h2')
+    for row in rows:
+        row.get_text()
+    return render_template('index.html',**locals())
+
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
